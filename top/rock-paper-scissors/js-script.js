4201d79de843b99e3f3ce35a471600e651de1bab
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-10-26 07:38:25 -0700

    Not much progress.  End of morning 10.26

--- top/rock-paper-scissors/js-script.js
+++ top/rock-paper-scissors/js-script.js
@@ -1,20 +1,16 @@
 // https://www.theodinproject.com/lessons/foundations-rock-paper-scissors#assignment
-// Function to randomly return either 'Rock', 'Paper', or 'Scissors' for Computer.
-function getComputerChoice() {
-    let gameArray = ['Rock', 'Paper', 'Scissors'];  
-    return gameArray[Math.floor((Math.random() * gameArray.length))];
-}
-
 
 // Function which asks player to choose "Rock, Paper, or Scissors" with JS prompt() call.  This function also makes the answer case-insensitive, and also verifies that it is one of three acceptable answers.
 function getPlayerSelection() {
-    // array of "accepted" answers.
+    // Array of "accepted" answers.
     const acceptedAnswers = ["rock", "paper", "scissors"];
 
     // Player inputs either: Rock, Paper, or Scissors and toLowerCase() makes it case-insensitive.
-    let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
 
-    // verification of player input.  Checking array of accepted values against the player selection.  Then makes selection's first letter capitalized.
+    // Verification of player input.  
+    // Checking player selection against accepted values in array.
+    // Then makes selection's first letter capitalized.
     if (acceptedAnswers.includes(playerSelection)) {
         let str = playerSelection;
         let capatalizedAnswer = str.charAt(0).toUpperCase() + str.slice(1);
@@ -25,58 +21,51 @@ function getPlayerSelection() {
 }
 }
 
+// Function to randomly return either 'Rock', 'Paper', or 'Scissors' for Computer.
+function getComputerChoice() {
+    let gameArray = ['Rock', 'Paper', 'Scissors'];  
+    return gameArray[Math.floor((Math.random() * gameArray.length))];
+}
+
+numberOfPlayerWins = 0;
+numberOfComputerWins = 0;
+
 // Function to play a single round of the game Rock, Paper, Scissors.
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors" || 
+    playerSelection === "Paper" && computerSelection === "Rock" ||
+    playerSelection === "Scissors" && computerSelection === "Paper") {
+        ++numberOfPlayerWins;
+        return `You Win! ${playerSelection} beats ${computerSelection} You:${numberOfPlayerWins}, Computer:${numberOfComputerWins}`; 
+    } else if (playerSelection === computerSelection) {
+        return `You Tied! You both selected ${playerSelection}`; 
+    } else {
+        ++numberOfComputerWins;
+        return `You Lose. ${computerSelection} beats ${playerSelection}`;
+    }
+}
 
-// function playRound (playerSelection, computerSelection) {
-//     if (playerSelection === "Rock" && computerSelection === "Scissors" || 
-//     playerSelection === "Paper" && computerSelection === "Rock" ||
-//     playerSelection === "Scissors" && computerSelection === "Paper") {
-//         return `You Win! ${playerSelection} beats ${computerSelection}`; 
-//     } else if (playerSelection === computerSelection) {
-//         return `You Tied! You both selected ${playerSelection}`; 
-//     } else {
-//         return `You Lose. ${computerSelection} beats ${playerSelection}`;
-//     }
-// }
+let playerSelection = getPlayerSelection();
+let computerSelection = getComputerChoice();
+console.log(playerSelection); // Checking player selection.
+console.log(computerSelection); // Checking computer selection.
 
-// let computerSelection = getComputerChoice();
-// let playerSelection = getPlayerSelection();
-// console.log(computerSelection);
-// console.log(playerSelection);
 // console.log(playRound(playerSelection, computerSelection));
 
-
-// Function to play Rock Paper Scissors 5 times.
-// Should keep score and report winner or loser at the end.
+console.log(`Player Wins: ${numberOfPlayerWins}`);
+console.log(`Computer Wins: ${numberOfComputerWins}`);
 
 // Thoughts: TOP suggests using a for loop.  Need to brush up on this.
 // Need to run the game 5 times then call it over.  Also need to display the winner overall. Hmmm.
 
-function game() {
-    for (let i = 0; i < 5, i++) {
-        if (i < 2) {
-            playRound();
-        } else {
-            return console.log("Game Over");
-        }
+// Function to play "Rock, Paper, Scissors" Game 5 Times and report winner.
 
-
-        let computerSelection = getComputerChoice();
-        let playerSelection = getPlayerSelection();
-        console.log(computerSelection);
-        console.log(playerSelection);
+for (let i = 0; i < 5; i++) {
+    function game() {
         console.log(playRound(playerSelection, computerSelection));
-
-        function playRound(playerSelection, computerSelection) {
-            if (playerSelection === "Rock" && computerSelection === "Scissors" || 
-            playerSelection === "Paper" && computerSelection === "Rock" ||
-            playerSelection === "Scissors" && computerSelection === "Paper") {
-                return `You Win! ${playerSelection} beats ${computerSelection}`; 
-            } else if (playerSelection === computerSelection) {
-                return `You Tied! You both selected ${playerSelection}`; 
-            } else {
-                return `You Lose. ${computerSelection} beats ${playerSelection}`;
-            }
-        }
     }
-}
+        
+    }
+
+
+console.log(game());
