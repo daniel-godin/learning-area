7bf5cdf957515a9a84c2fb5a5c56be2895250b82
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-10-27 18:14:23 -0700

    Mostly works, need to fix validation of a correct answer and have a way to loop back until a correct one is given

--- top/rock-paper-scissors/js-script.js
+++ top/rock-paper-scissors/js-script.js
@@ -11,96 +11,79 @@ function getComputerChoice() {
 
 
 function game() {
+
+    let numberOfPlayerWins = 0;
+    let numberOfComputerWins = 0;
+
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
-        let computerSelection = getComputerChoice();  
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-        const acceptedAnswers = ["Rock", "Paper", "Scissors"];
-        if (acceptedAnswers.includes(playerSelection)) {
-            console.log(playRound(playerSelection, computerSelection));
+        let computerSelection = getComputerChoice(); 
+        let playerSelection = playerInputVerification(); 
+
+        // let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
+        // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+        playRound(playerSelection, computerSelection);
+
+        if (playRound(playerSelection, computerSelection) === `playerWinsRound`) {
+            ++numberOfPlayerWins;
+            console.log(`You won this round!  ${playerSelection} beats ${computerSelection}.`)
+        } else if (playRound(playerSelection, computerSelection) === `tieRound`) {
+            console.log(`You tied this round.  You both chose ${playerSelection}.`); 
         } else {
-            console.log(`Input Not Accepted`);
-        } 
+            ++numberOfComputerWins;
+            console.log(`You lose this round.  ${computerSelection} beats ${playerSelection}.`);
+        }
+        
+        console.log(`Score: P${numberOfPlayerWins} vs. C${numberOfComputerWins}.`);
+    }
+
+    function checkScoreFunction(numberOfPlayerWins, numberOfComputerWins) {
+        if (numberOfPlayerWins > numberOfComputerWins) {
+            return `You win the game!  Player Score:${numberOfPlayerWins} vs. Computer Score:${numberOfComputerWins}.`;
+        } else if (numberOfComputerWins > numberOfPlayerWins) {
+            return `You lose the game.  Computer Score:${numberOfComputerWins} vs. Player Score:${numberOfPlayerWins}.`;
+        } else {
+            return `Tie Game!  Try Again!  Player Score:${numberOfPlayerWins} vs. Computer Score:${numberOfComputerWins}.`;
+        }
+    }
+
+    console.log(checkScoreFunction(numberOfPlayerWins, numberOfComputerWins));
+}
+
+function playerInputVerification () {
+    
+    
+    let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    // Get input from user with prompt().
+    // Check if input matches one of the acceptedAnswers array values.
+    // If it does match, return that value
+    // If it does not match, do another prompt() until it does, or you have 
+    const acceptedAnswers = ["Rock", "Paper", "Scissors"];
+    
+// ------------ Need To Fix This Part.  The Verification Isn't Working As I Want -----------
+
+    if (acceptedAnswers.includes(playerSelection)) {
+        return playerSelection;
+    } else {
+        let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     }
 }
 
 // Function to play a single round of the game Rock, Paper, Scissors.
 function playRound(playerSelection, computerSelection) {
-    // playerSelection = playerSelection.toLowerCase();
-    // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    
     if (playerSelection === "Rock" && computerSelection === "Scissors" || 
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissors" && computerSelection === "Paper") {
-            return `You Win! ${playerSelection} beats ${computerSelection}.`; 
+            return `playerWinsRound`;
+            // return `You Win! ${playerSelection} beats ${computerSelection}. Wins: Player(${numberOfPlayerWins}), Computer(${numberOfComputerWins}).`; 
     } else if (playerSelection === computerSelection) {
-            return `You Tied! You Both Chose ${playerSelection}.`; 
+            return `tieRound`;
+            // return `You Tied! You Both Chose ${playerSelection}. Wins: Player(${numberOfPlayerWins}), Computer(${numberOfComputerWins}).`; 
     } else {
-            return `You Lose. ${computerSelection} beats ${playerSelection}.`;
+            return `computerWinsRound`;
+            // return `You Lose. ${computerSelection} beats ${playerSelection}. Wins: Player(${numberOfPlayerWins}), Computer(${numberOfComputerWins}).`;
     }
 }
-
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-
-// Function which asks player to choose "Rock, Paper, or Scissors" with JS prompt() call.  This function also makes the answer case-insensitive, and also verifies that it is one of three acceptable answers.
-// function getPlayerChoice() {
-//     // Array of "accepted" answers.
-//     const acceptedAnswers = ["rock", "paper", "scissors"];
-
-//     // Player inputs either: Rock, Paper, or Scissors and toLowerCase() makes it case-insensitive.
-//     let playerChoiceAnswer = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
-
-//     // Verification of player input.  
-//     // Checking player choice against accepted values in array.
-//     // Then makes choice's first letter capitalized.
-//     if (acceptedAnswers.includes(playerChoiceAnswer)) {
-//         let str = playerChoiceAnswer;
-//         let capatalizedAnswer = str.charAt(0).toUpperCase() + str.slice(1);
-//         return capatalizedAnswer;
-//     } else {
-//     // Accepted answer should make the game run.  Not accept should prompt again to make player choose the 3 choices.
-//     return "Input misspelled";
-// }
-// }
-
-//     const acceptedAnswers = ["Rock", "Paper", "Scissors"];
-// if (acceptedAnswers.includes(playerSelection)) {
-
-
-
-
-
-
-
-// Function to play "Rock, Paper, Scissors" Game 5 Times and report winner.
-// Thoughts: TOP suggests using a for loop.  Need to brush up on this.
-// Need to run the game 5 times then call it over.  Also need to display the winner overall. Hmmm.
-
-
-
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(); // Function which plays 1 round of the game.
-
-//         // getPlayerChoice(); // Function which prompts player for choice.
-//         // getComputerChoice(); // Function which randomly chooses for computer.
-
-//         let playerChoice = getPlayerChoice(); // Making playerChoice variable from the return of getPlayerChoice() function.
-//         let computerChoice = getComputerChoice(); // Making computerChoice variable from the return of getComputerChoice() function.
-
-//         return `Player Choice:  ${playerChoice}.  Computer Choice:  ${computerChoice}.` // Returns values of player and computer.
-
-//         // numberOfPlayerWins = 0; // Variable for players wins that starts at 0.
-//         // ++numberOfPlayerWins;
-//         // numberOfComputerWins = 0; // Variable for computer wins that starts at 0.
-//         // ++numberOfComputerWins
-//         // console.log(`Player Wins: ${numberOfPlayerWins}`); // Console Test to see if wins are calculating.
-//         // console.log(`Computer Wins: ${numberOfComputerWins}`); // Console Test to see if wins are calculating.
-//     }
-// }
-
-// console.log(game());
-
